d70a39376d598ed15ee236c51d6be83e3350bda9
Doniben <doniben@esperanto.co>
2022-06-02 23:59:48 -0500

    String concatenation ways

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,19 @@
+// CONCATENAR
+
+stringBase = "Primera parte del string  ";
+stringConcat = "Segunda parte del string";
+
+// concatenar con método .concat()
+console.log(stringBase.concat(stringConcat));
+
+// Conctatenar con +
+
+console.log("concatenar con +:" + stringBase + stringConcat);
+
+// Concatenar con ,
+
+console.log("concatenar con ,:" + stringBase, stringConcat);
+
+// Concatenar con teplate string: backticks ``
+
+console.log(`Concatenar con teplate string: ${stringBase} ${stringConcat}`);
