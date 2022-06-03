1b899282064b871ca470bd73a95777d488da7551
Doniben <doniben@esperanto.co>
2022-06-02 23:59:11 -0500

    String methods: length, includes, indexOf

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,15 @@
+// Métodos para los Strings
+
+const stringMethods = "Hola con doble comilla";
+
+// Contar caracteres del string. Length es una proiedad de JS. Podemos crear nuestras propias propiedades
+
+console.log(stringMethods.length);
+
+// includes(). Verifica si un string contiene una palabra o caracter devolviendo un booleano
+
+console.log(stringMethods.includes("Hola"));
+
+// indexOf(). Verifica la posición en que se encuentre una palabra dentro del string
+
+console.log(stringMethods.indexOf("Hola"));
