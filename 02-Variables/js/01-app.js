36db7e738a138f579208020006a1455e4251f2b9
Doniben <doniben@esperanto.co>
2022-06-02 23:58:24 -0500

    String types

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,13 @@
+// Tres maneras de crear strings en JS
+
+const string1 = "Hola con doble comilla";
+const string2 = 'Hola con comilla simple';
+const string3 = String("Hola con String");
+const string4 = new String("Hola con String instanciado");
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(string4);
+
+ 
